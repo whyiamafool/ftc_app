9284a44e22b8b2142dc0ab8b87ef1dc955496335
Pranav Chitiveli <39918567+PVCPipe@users.noreply.github.com>
2019-06-30 19:38:08 -0400

    update DemoOp
    Changed throttle to triggers, added steering turns. Steering turns yet to be tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOp.java
@@ -29,23 +29,23 @@ public class DemoOp extends LinearOpMode {
         while (opModeIsActive()) {
 
             //gamepad 1 (logitech)
-            double throttle = -gamepad1.left_stick_y;
-            double steering = gamepad1.left_stick_x/2;
+            double throttle = (gamepad1.right_trigger) - (gamepad1.left_trigger);
+            double steering = -gamepad1.left_stick_x/2;
 
             double rPower = throttle - steering;
             double lPower = throttle + steering;
 
-            if (steering != 0) {
-                lPower /= 2.5;
-                rPower /= 2.5;
-            } else {
-                lPower /= 2;
-                rPower /= 2;
+            if (throttle > 0) {
+                if (steering > 0) {
+                    lPower += steering;
+                } else if (steering < 0) {
+                    rPower += steering;
+                }
             }
 
             //gamepad 1 (logitech) setPower
-            robot.left_drive.setPower(lPower/4);
-            robot.right_drive.setPower(rPower/4);
+            robot.left_drive.setPower(lPower);
+            robot.right_drive.setPower(rPower);
 
             telemetry.addData("Status", "Running");
             telemetry.update();

c0b3d01e8066505e0289279e930dc0e805ef31b9
Pranav Chitiveli <39918567+PVCPipe@users.noreply.github.com>
2019-07-14 12:09:27 -0400

    made it easier to understand (for me)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOp.java
@@ -15,27 +15,32 @@ public class MecanumOp extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-        
-        double lFrontPow = gamepad1.left_stick_y - (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
-        double lRearPow  = gamepad1.left_stick_y + (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
-        double rFrontPow = gamepad1.left_stick_y + (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
-        double rRearPow  = gamepad1.left_stick_y - (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
 
-        double[] wheelPows = {lFrontPow,lRearPow,rFrontPow,rRearPow};
+        double fLeftPow = gamepad1.left_stick_y - (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
+        double rLeftPow  = gamepad1.left_stick_y + (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
+        double fRightPow = gamepad1.left_stick_y + (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
+        double rRightPow  = gamepad1.left_stick_y - (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
+
+        double[] wheelPows = {fLeftPow,rLeftPow,fRightPow,rRightPow};
 
         Arrays.sort(wheelPows);
 
         if (wheelPows[3] > 1) {
-            lFrontPow /= wheelPows[3];
-            lRearPow /= wheelPows[3];
-            rFrontPow /= wheelPows[3];
-            rRearPow /= wheelPows[3];
+            fLeftPow /= wheelPows[3];
+            rLeftPow /= wheelPows[3];
+            fRightPow /= wheelPows[3];
+            rLeftPow /= wheelPows[3];
         }
 
         // set motor powers
-        robot.frontLeft.setPower(lFrontPow);
-        robot.frontRight.setPower(rFrontPow);
-        robot.rearLeft.setPower(lRearPow);
-        robot.rearRight.setPower(rRearPow);
+        robot.frontLeft.setPower(fLeftPow);
+        robot.frontRight.setPower(rLeftPow);
+        robot.rearLeft.setPower(fRightPow);
+        robot.rearRight.setPower(rRightPow);
+
+        telemetry.addData("FrontLeftPow",fLeftPow);
+        telemetry.addData("FrontRightPow",rRightPow);
+        telemetry.addData("RearLeftPow",fRightPow);
+        telemetry.addData("RearRightPow",rRightPow);
     }
 }
